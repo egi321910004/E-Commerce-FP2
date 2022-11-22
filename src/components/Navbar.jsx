a26f4262cbcc76a3bdd1c1d2480c23d2e4a4eb12
Egi rizki ramadani <71552621+egi321910004@users.noreply.github.com>
2022-11-22 23:19:19 +0700

    Refactoring
    -Refactoring

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -19,7 +19,13 @@ export default function Navbar() {
             >
               Home
             </Link>
-
+            <Link
+              as={Link}
+              to="/Rekap"
+              class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+            >
+              Rekap
+            </Link>
             <Link
               as={Link}
               to="/login"
