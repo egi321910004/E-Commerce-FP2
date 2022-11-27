15312907882734265942ac3d813e2c60b3c20558
Egi Rizki Ramadani <egi.rizki@megafinance.co.id>
2022-11-28 01:29:18 +0700

    add authSlice

--- src/Redux/Store.js
+++ src/Redux/Store.js
@@ -3,9 +3,11 @@ import { persistReducer, persistStore } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import ProductsReducer from "./ProductsSlice";
 import CartReducer from "./CartSlice";
+import userSlice from "./authSlice";
 
 const reducers = combineReducers({
   products: ProductsReducer,
+  auth: userSlice,
   cart: CartReducer,
 });
 
